89b5df855046c23ea6cc142a19d60d35131a3f9d
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-10-10 07:29:26 +0300

    Conditional statements

---
+++ 03.conditional_statements/training.py
@@ -0,0 +1,19 @@
+import math
+# down = math.floor(45.67)
+# print(down)
+# up = math.ceil(45.67)
+# print(up)
+
+# down = round(1.50)
+# print(down)
+# down = round(2.50)
+# print(down)
+
+# number = 5
+# print(number)
+# number = -5
+# print(abs(number))    #abs
+
+
+
+
