4df428b7f8564a85c3867474cf4c98100fc7e2ba
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-10-27 22:02:08 +0300

    Conditional statements - advanced

---
+++ 05.conditional_statements_advanced/09_Fruit _or_Vegetable.py
@@ -0,0 +1,24 @@
+product = input()
+type_of_product = "unknown"
+if product == "banana":
+    type_of_product = "fruit"
+if product == "apple":
+    type_of_product = "fruit"
+if product == "kiwi":
+    type_of_product = "fruit"
+if product == "cherry":
+    type_of_product = "fruit"
+if product == "lemon":
+    type_of_product = "fruit"
+if product == "grapes":
+    type_of_product = "fruit"
+if product == "tomato":
+    type_of_product = "vegetable"
+if product == "pepper":
+    type_of_product = "vegetable"
+if product == "cucumber":
+    type_of_product = "vegetable"
+if product == "carrot":
+    type_of_product = "vegetable"
+
+print(type_of_product)
---
+++ 05.conditional_statements_advanced/10_Invalid_Number.py
@@ -0,0 +1,7 @@
+
+number = int(input())
+if (number < 100 or number > 200) and number != 0:
+    print("invalid")
+
+
+
