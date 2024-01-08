83c72718ad6a17688fa00581d93e678a0663d17b
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2024-01-08 17:41:59 +0200

    For loop

---
+++ 07.for_loop/07_Sum_Numbers.py
@@ -0,0 +1,8 @@
+n = int(input())
+sum_numbers = 0
+
+for _ in range(n):
+    num = int(input())
+    sum_numbers += num
+
+print(sum_numbers)
---
+++ 07.for_loop/10. Odd Even Sum.py
@@ -0,0 +1,18 @@
+n = int(input())
+sum_even = 0
+sum_odd = 0
+
+for i in range(n):
+    num = int(input())
+    if i % 2 == 0:
+        sum_even += num
+    else:
+        sum_odd += num
+
+if sum_even == sum_odd:
+    print("Yes")
+    print("Sum =", sum_even)
+else:
+    diff = abs(sum_even - sum_odd)
+    print("No")
+    print("Diff =", diff)
